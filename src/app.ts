fbff64d21e9fd6bdafb0446b14872e285595e0b5
Mateus Iasbik <mateusmpi@gmail.com>
2025-10-31 17:51:17 -0300

    feat: add flights router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from 'express';
+import flightsRouter from "./routers/flights-router.js";
+import errorHandlingMiddleware from "./middlewares/errors-middleware.js";
 
 const app = express();
+app.use(express.json());
 
 app.get('/health', (req: Request, res: Response) => {
   res.send('Backend Pilops funcionando!');
 });
+
+app.use("/flights", flightsRouter);
+
+app.use(errorHandlingMiddleware);
 
 export default app;
--- src/routers/flights-router.ts
+++ src/routers/flights-router.ts
@@ -1,1 +1,10 @@
+import { Router } from 'express';
+import { flightsController } from '../controllers/flights-controller.js';
 
+const flightsRouter = Router();
+
+flightsRouter.get('/', flightsController.getAllFlights);
+flightsRouter.get('/total-balance/aircraft', flightsController.getTotalBalanceByAircraft);
+flightsRouter.get('/:id', flightsController.getFlightById);
+
+export default flightsRouter;
