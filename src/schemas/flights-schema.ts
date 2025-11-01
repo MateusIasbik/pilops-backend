6c19acc77b4dab7e7dacbd47ccf5355e4ea6339e
Mateus Iasbik <mateusmpi@gmail.com>
2025-11-01 09:08:42 -0300

    test deploy 8

--- src/schemas/flights-schema.ts
+++ src/schemas/flights-schema.ts
@@ -1,32 +1,32 @@
-import Joi from "joi";
+// import Joi from "joi";
 
-const aircraftSchema = Joi.object({
-  name: Joi.string().required(),
-  registration: Joi.string().required(),
-  airline: Joi.string().required(),
-});
+// const aircraftSchema = Joi.object({
+//   name: Joi.string().required(),
+//   registration: Joi.string().required(),
+//   airline: Joi.string().required(),
+// });
 
-const routeSchema = Joi.object({
-  from: Joi.string().required(),
-  to: Joi.string().required(),
-});
+// const routeSchema = Joi.object({
+//   from: Joi.string().required(),
+//   to: Joi.string().required(),
+// });
 
-const flightDataSchema = Joi.object({
-  date: Joi.date().iso().required(),
-  balance: Joi.number().required(),
-  route: routeSchema.required(),
-  xp: Joi.number().required(),
-  missionBonus: Joi.number().required(),
-});
+// const flightDataSchema = Joi.object({
+//   date: Joi.date().iso().required(),
+//   balance: Joi.number().required(),
+//   route: routeSchema.required(),
+//   xp: Joi.number().required(),
+//   missionBonus: Joi.number().required(),
+// });
 
-const flightSchema = Joi.object({
-  id: Joi.string().required(),
-  aircraft: aircraftSchema.required(),
-  flightData: flightDataSchema.required(),
-});
+// const flightSchema = Joi.object({
+//   id: Joi.string().required(),
+//   aircraft: aircraftSchema.required(),
+//   flightData: flightDataSchema.required(),
+// });
 
-const flightsResponseSchema = Joi.object({
-  flights: Joi.array().items(flightSchema).required(),
-});
+// const flightsResponseSchema = Joi.object({
+//   flights: Joi.array().items(flightSchema).required(),
+// });
 
-export { flightsResponseSchema, flightSchema, flightDataSchema, routeSchema, aircraftSchema };
+// export { flightsResponseSchema, flightSchema, flightDataSchema, routeSchema, aircraftSchema };
