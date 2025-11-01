8c8cc875e005e782a3d58fab825756e7b2bb2131
Mateus Iasbik <mateusmpi@gmail.com>
2025-11-01 09:51:17 -0300

    test deploy 16

--- src/middlewares/schema-middleware.ts
+++ src/middlewares/schema-middleware.ts
@@ -1,15 +1,15 @@
-import { NextFunction, Request, Response } from "express";
-import httpStatus from "http-status";
-import Joi, { ObjectSchema, ValidationErrorItem } from "joi";
+// import { NextFunction, Request, Response } from "express";
+// import httpStatus from "http-status";
+// import Joi, { ObjectSchema, ValidationErrorItem } from "joi";
 
-export function validateSchema(schema: ObjectSchema) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const validation = schema.validate(req.body, { abortEarly: false });
-    if (validation.error) {
-      return res
-        .status(httpStatus.UNPROCESSABLE_ENTITY)
-        .send(validation.error.details.map((detail: ValidationErrorItem) => detail.message));
-    }
-    next();
-  };
-}
+// export function validateSchema(schema: ObjectSchema) {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     const validation = schema.validate(req.body, { abortEarly: false });
+//     if (validation.error) {
+//       return res
+//         .status(httpStatus.UNPROCESSABLE_ENTITY)
+//         .send(validation.error.details.map((detail: ValidationErrorItem) => detail.message));
+//     }
+//     next();
+//   };
+// }
